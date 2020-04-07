f4a949f27d9e57df521681e0afd05fa6a0a1c04b
Reevo <radoslawkarbowiak@gmail.com>
2020-04-07 23:46:39 +0200

    Added anonymous readings

--- server.py
+++ server.py
@@ -11,9 +11,26 @@ def connect_to_broker():
     server.loop_start()
     server.subscribe("terminal")
 
+def checkCursorEmpty(cursor):
+    for cursor in cursor.fetchall():
+        return False
+    return True
+
 def disconnect():
     server.loop_stop()
     server.disconnect()
+
+def addReading(cursor, decodedMessage, date):
+    for card in cursor.fetchall():
+        idCard = card[0]
+
+    data = (date, str(decodedMessage[2]), str(idCard))
+    cursor.execute("INSERT INTO Readings(Date, WentIn, IdCard) VALUES (?, ?, ?)", data)
+    print("Dodanie odczytu przebiegło pomyślnie")
+
+def addAnonymousCard(cursor, rfid):
+    cursor.execute("INSERT INTO Cards (Identifier) VALUES (?)", (rfid,))
+    
 
 def receive_message(client, data, message):
     message_decoded = (str(message.payload.decode("utf-8"))).split(".")
@@ -26,13 +43,14 @@ def receive_message(client, data, message):
         conn = db.connect('idReaderDatabase.db')
         c = conn.cursor()   
         c.execute("SELECT * FROM Cards WHERE Identifier = ?", (message_decoded[0],))
-        for card in c.fetchall():
-            print(card[0])
-            idCard = card[0]
 
-        data = (date, str(message_decoded[2]), str(idCard))
-        c.execute("INSERT INTO Readings(Date, WentIn, IdCard) VALUES (?, ?, ?)", data)
-        print("Dodanie odczytu przebiegło pomyślnie")
+        if not checkCursorEmpty(c):
+            addReading(c, message_decoded, date)
+        else: 
+            addAnonymousCard(c, message_decoded[0])
+            c.execute("SELECT * FROM Cards WHERE Identifier = ?", (message_decoded[0],))
+            addReading(c, message_decoded, date)
+        
         conn.commit()
     else:
         print(message_decoded[0] + ":" + message_decoded[1]) 
